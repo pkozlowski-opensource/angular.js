2f9fe72e3d5412e6c79cec161282d3f53c53726e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-07-18 20:44:27 +0200

    fix(form): pick the right attribute name for ngForm

--- test/ng/directive/formSpec.js
+++ test/ng/directive/formSpec.js
@@ -63,6 +63,17 @@ describe('form', function() {
     expect(scope.myForm.alias).toBeDefined();
   });
 
+  it('should use ngForm value as form name when nested inside form', function () {
+    doc = $compile(
+      '<form name="myForm">' +
+        '<div ng-form="ngForm"><input type="text" name="alias" ng-model="value"/></div>' +
+      '</form>')(scope);
+
+    expect(scope.myForm).toBeDefined();
+    expect(scope.myForm.ngForm).toBeDefined();
+    expect(scope.myForm.ngForm.alias).toBeDefined();
+  });
+
 
   it('should publish form to scope when name attr is defined', function() {
     doc = $compile('<form name="myForm"></form>')(scope);
